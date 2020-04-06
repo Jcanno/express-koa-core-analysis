3e925cbb2d8db5ebfc8d0e177a54f7d74612c44f
Jacano <641283231@qq.com>
2020-04-06 18:26:43 +0800

    feat:add simple-express

--- simple-express/index.js
+++ simple-express/index.js
@@ -1,12 +1,6 @@
 const express = require('./express');
 const app = express();
 
-app.get('/sds', (req, res, next) => {
-	console.log('2222');
-	res.end('hi')
-	next()
-})
-
 app.get('/', (req, res) => {
 	console.log('监听到请求');
 	res.end('hello')
